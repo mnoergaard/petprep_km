ad3411f07e004b75416f87c9bc7ded20025628e6
mnoergaard <martin.noergaard@nru.dk>
2025-07-26 21:37:46 +0200

    FIX: update import

--- petprep_km/interfaces/io.py
+++ petprep_km/interfaces/io.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def load_tacs(tacs_tsv_path):
     df = pd.read_csv(tacs_tsv_path, sep='\t')
-    tac_times = (df['frame_start'] + df['frame_end']) / 2
+    tac_times = (df.iloc[:, 0] + df.iloc[:, 1]) / 2  # First and second columns
     roi_names = df.columns[2:]
-    tac_values = df[roi_names].values.T  # Shape: regions x frames
+    tac_values = df.iloc[:, 2:].values.T  # Shape: regions x frames
     return tac_times.values, roi_names.tolist(), tac_values
 
 def load_blood(blood_tsv_path):
